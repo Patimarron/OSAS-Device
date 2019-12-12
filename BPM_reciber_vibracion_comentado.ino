fd0b018b1b0eee958901912d71fe2b02217e586c
Patimarron <57150936+Patimarron@users.noreply.github.com>
2019-12-12 17:29:22 +0100

    Add files via upload

---
+++ BPM_reciber_vibracion_comentado.ino
@@ -0,0 +1,63 @@
+//First we Include Libraries for the wireless module library
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+//create an RF24 object which forms the wireless module
+RF24 radio(9, 8);  // CE, CSN
+
+//We specify the address through which both the wireless modules communicate, with a 5 digit password.
+const byte address[6] = "00005";
+//We declare a variable for the motor of vibration which is connected to pin number 10
+int motorvibracion = 10;
+
+void setup()
+{
+  while (!Serial);
+  Serial.begin(9600);// Begin the Serial Monitor at 9600
+
+  radio.begin();//Start the comunication between modules
+
+  // We set the address defined before
+  radio.openReadingPipe(0, address);
+
+  //We set this module as the reciber of information
+  radio.startListening();
+
+  //We declare the vibration monitor as an output
+  pinMode(motorvibracion, OUTPUT);
+  //The vibration motor starts turned of
+  digitalWrite(motorvibracion, LOW);
+}
+
+void loop()
+{
+  analogWrite(motorvibracion, 0); // Vibration motor its still
+  int myBPM; // We declare the variable BPM
+  //Read the data if available in buffer
+  if (radio.available())
+  {
+    //We read the iformation reciebed from the transmitter of the BPM of the user
+    radio.read(&myBPM, sizeof(myBPM));
+    //Print the value of the variable
+    Serial.println(myBPM);
+  }//If BPM is equal to 0 do not do anything because it is useless information
+  if (myBPM == 0) {
+
+  } else if (myBPM < 40) {// If the Beats per  minute are too low do the following:
+    digitalWrite(5, LOW);//Green led of the RGB led turns off
+    digitalWrite(3, LOW);//Red led of the RGB led turns off
+    digitalWrite (6, HIGH);//BLue led of the RGB led turns on
+    analogWrite(motorvibracion, 250);//The vibration motor turns on to a power of 250(which is the maximun)
+  } else if (myBPM > 120) {// If the Beats per  minute are too high do the following:
+    digitalWrite(5, LOW);//Green led of the RGB led turns off
+    digitalWrite(6, LOW);//BLue led of the RGB led turns off
+    digitalWrite (3, HIGH);//Red led of the RGB led turns on
+    analogWrite(motorvibracion, 200);//The vibration motor turns on to a power of 200
+  } else if (myBPM > 40 && myBPM < 120) {//If the Beats per  minute are normal do the following:
+    digitalWrite(3, LOW);//Red led of the RGB led turns off
+    digitalWrite(6, LOW);//BLue led of the RGB led turns off
+    digitalWrite (5, HIGH);//Green led of the RGB led turns on
+    analogWrite(motorvibracion, 0);//The vibration motor turns off 
+  }
+}
---
+++ BPM_transmisor_comentado.ino
@@ -0,0 +1,72 @@
+//First we Include Libraries for the wireless module library
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+//create an RF24 object which forms the wireless module
+RF24 radio(9, 8);  // CE, CSN
+
+//We specify the address through which both the wireless modules communicate, with a 5 digit password.
+const byte address[6] = "00005";
+
+
+#define USE_ARDUINO_INTERRUPTS true    // Set-up low-level interrupts for most acurate BPM math.
+#include <PulseSensorPlayground.h>     // Includes the PulseSensorPlayground Library.   
+
+//  Variables
+const int PulseWire = 0;       // PulseSensor firt cable connected to analog pin 0.
+int LED;                       // We declare a varibale for the RGB led
+int Threshold = 512;           // Determine which Signal to "count as a beat" and which to ignore, all the numbers below will this constant will be ignored.
+
+PulseSensorPlayground pulseSensor;  // Creates an instance of the PulseSensorPlayground object called "pulseSensor"
+
+
+void setup()
+{
+  Serial.begin(9600);          // Begin the Serial Monitor at 9600
+
+
+  pulseSensor.analogInput(PulseWire);//We configurate the pulseSensor module aranging variables
+  pulseSensor.setThreshold(Threshold);
+
+  // Begin the PulseSensor module and confirm with a sentence to have and output and make sure it is working.
+  if (pulseSensor.begin()) {
+    Serial.println("We created a pulseSensor Object !");  //Print this sentence as confirmation
+  }
+  radio.begin(); //Start the comunication between modules
+
+  // We set the address defined before
+  radio.openWritingPipe(address);
+
+  //We set this module as the transmitter of information
+  radio.stopListening();
+}
+void loop()
+{
+  int myBPM = pulseSensor.getBeatsPerMinute(); // We create the variable myBPM which counts the beats per minute of the user
+
+  if (pulseSensor.sawStartOfBeat()) {            // Check if a heart beat is happening repeatedly
+    Serial.println("♥ beat ♥"); // If a heart beat does happen, print the sentence shown before and the value of the BPM
+    Serial.print("BPM: ");
+    Serial.println(myBPM);
+  }
+  pulseSensor.blinkOnPulse(LED); // We declare that the RGB led should blink at the rhythm of the heart of the user.
+  if (myBPM < 40) { // If the Beats per  minute are too low do the following:
+    digitalWrite(5, LOW); //Green led of the RGB led turns off
+    digitalWrite(3, LOW);//Red led of the RGB led turns off
+    LED = 6; //BLue led of the RGB led turns on
+  } else if (myBPM > 120) {// If the Beats per  minute are too high do the following:
+    digitalWrite(5, LOW);//Green led of the RGB led turns off
+    digitalWrite(6, LOW);//BLue led of the RGB led turns off
+    LED = 3;//Red led of the RGB led turns on
+  } else {// If the Beats per  minute are normal do the following:
+    digitalWrite(6, LOW);//BLue led of the RGB led turns off
+    digitalWrite(3, LOW);//Red led of the RGB led turns off
+    LED = 5;//Green led of the RGB led turns on
+  }
+  delay(10); // Wait for 10
+  {
+    radio.write(&myBPM, sizeof(myBPM)); //Write the value of BPM on the wireless module, to comunicate with the reciever
+    delay(100);// Wait for 100
+  }
+}
